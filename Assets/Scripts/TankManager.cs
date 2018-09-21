5ba54035adae58a08d48ca04e217f8fe06b1c2d2
Julia Shuieh <crystallineblade@gmail.com>
2018-09-20 20:30:38 -0700

    added project

---
+++ Assets/Scripts/TankManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TankManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    void MoveLeft()
+    {
+
+    }
+
+    void MoveRight()
+    {
+
+    }
+
+    void Jump()
+    {
+
+    }
+
+    void RotateBarrelUp()
+    {
+
+    }
+
+    void RotateBarrelDown()
+    {
+
+    }
+
+    void Shoot()
+    {
+
+    }
+}
